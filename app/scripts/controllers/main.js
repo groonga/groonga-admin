2487fced995f5c367ea1cc379aef1ebbb9f04288
Kouhei Sutou <kou@clear-code.com>
2014-10-21 18:22:40 +0900

    Scaffold
        % yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name groongaAdminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the groongaAdminApp
+ */
+angular.module('groongaAdminApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
