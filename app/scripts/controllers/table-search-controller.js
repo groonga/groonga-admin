4f88c9ef8787fec357d15fe4eae31f42090b2bb1
Kouhei Sutou <kou@clear-code.com>
2014-10-23 11:47:44 +0900

    Support --match_columns and --query

--- app/scripts/controllers/table-search-controller.js
+++ app/scripts/controllers/table-search-controller.js
@@ -8,7 +8,7 @@
  * Controller of the groongaAdminApp
  */
 angular.module('groongaAdminApp')
-  .controller('TableSearchController', function ($scope, $routeParams, $http) {
+  .controller('TableSearchController', function ($scope, $routeParams, $location, $http) {
     function escapeCommandValue(value) {
       return value.replace(/(["\\])/g, function(match) {
         return '\\' + match[1];
@@ -35,11 +35,26 @@ angular.module('groongaAdminApp')
     $scope.message = '';
     $scope.elapsedTimeInMilliseconds = 0;
     $scope.nTotalRecords = 0;
+    $scope.parameters = angular.copy($location.search());
+
+    $scope.search = function(parameters) {
+      $location.search(parameters);
+    };
+
+    $scope.clear = function() {
+      $location.search({});
+    };
 
     var parameters = {
       table: $scope.table,
       callback: 'JSON_CALLBACK'
     };
+    angular.forEach($scope.parameters, function(value, key) {
+      if (key in parameters) {
+        return;
+      }
+      parameters[key] = value;
+    });
     $http.jsonp('/d/select.json', {params: parameters})
       .success(function(data) {
         $scope.commandLine = buildCommandLine('select', parameters);
