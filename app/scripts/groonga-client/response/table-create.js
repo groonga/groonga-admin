8c0e4861337f770cfeee7c1ad374185b29fca644
Kouhei Sutou <kou@clear-code.com>
2014-12-31 23:42:02 +0900

    table_create: remove unused code

--- app/scripts/groonga-client/response/table-create.js
+++ app/scripts/groonga-client/response/table-create.js
@@ -9,15 +9,6 @@
   TableCreate.prototype = Object.create(GroongaClient.Response.Base.prototype);
   TableCreate.prototype.constructor = TableCreate;
 
-  TableCreate.prototype.parameters = function() {
-    return this.body()[0].map(function(parameter) {
-      return {
-        name: parameter[0],
-        type: parameter[1]
-      };
-    });
-  };
-
   TableCreate.prototype.isCreated = function() {
     return this.isSuccess() && this.body();
   };
