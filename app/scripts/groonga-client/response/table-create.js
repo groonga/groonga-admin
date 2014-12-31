13f6a68f4109adaaae016d3de5d998859b0d703d
Kouhei Sutou <kou@clear-code.com>
2014-12-31 23:18:00 +0900

    groonga-client: support table_create

---
+++ app/scripts/groonga-client/response/table-create.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function() {
+  function TableCreate(rawData) {
+    GroongaClient.Response.Base.call(this, rawData);
+  }
+  GroongaClient.Response.TableCreate = TableCreate;
+
+  TableCreate.prototype = Object.create(GroongaClient.Response.Base.prototype);
+  TableCreate.prototype.constructor = TableCreate;
+
+  TableCreate.prototype.parameters = function() {
+    return this.body()[0].map(function(parameter) {
+      return {
+        name: parameter[0],
+        type: parameter[1]
+      };
+    });
+  };
+
+  TableCreate.prototype.isCreated = function() {
+    return this.isSuccess() && this.body() == 1;
+  };
+})();
