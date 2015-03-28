d46e84b80341476a2439144b5df11ad0aff45c0c
Kouhei Sutou <kou@clear-code.com>
2015-03-28 14:15:38 +0900

    Don't use CDN
    Groonga Admin should not require remote network access.

--- Gruntfile.js
+++ Gruntfile.js
@@ -310,13 +310,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -462,7 +455,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'filerev',
     'usemin'
